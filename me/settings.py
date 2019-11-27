54cac39a967242d4f2e679649694d0f388bc9f17
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-11-26 22:44:20 -0300

    Changes

--- me/settings.py
+++ me/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
-    'portfolio.apps.PortfolioConfig'
+    'core', # Agregamos
+    'portfolio.apps.PortfolioConfig' # Importa la configuracion de la app portafolio
 
 ]
 
@@ -105,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
+# PARA CONFIGURAR LA REGION
 
 LANGUAGE_CODE = 'es-ar'
 
@@ -121,5 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# Declaramos variable de entorno
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media") # ruta interna
--- portfolio/apps.py
+++ portfolio/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class PortfolioConfig(AppConfig):
     name = 'portfolio'
     verbose_name = 'Portafolio' # Asignamos un nombre publico..
+
+# Configuracion de panel de control grafico.
