666f622222eef3a8964656d9e91001834019b137
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-03-25 05:20:09 -0300

    Test

--- portfolio/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Project
-
-# Create your views here.
-def portfolio(request):
-    projects = Project.objects.all()
-    print(type(projects))
-    return render(request, "portfolio/portfolio.html", {'projects':projects}) # Ultimo parametro es un diccionario
