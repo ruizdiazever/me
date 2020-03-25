b16662a411a7d813dc4566207101aa9b17481eb1
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-03-25 05:41:03 -0300

    Test II

--- me/settings.py
+++ me/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'me.wsgi.application'
 DATABASES = {
     #'default': {
     #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     #}
 
     'default': {
